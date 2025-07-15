b50744e343f2c830d58c2e542afd06482052104e
Piercillo <pierrecontreras92@gmail.com>
2025-07-14 23:08:07 -0500

    Primer commit

---
+++ src/app/components/experiencie/experiencie.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencie',
+  imports: [],
+  templateUrl: './experiencie.component.html',
+  styleUrl: './experiencie.component.scss'
+})
+export class ExperiencieComponent {
+
+}
